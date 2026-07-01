6a12377c80905c4ab74d46738ac974b50f07a48e
laoyutang <laoyutang00@gmail.com>
2026-07-01 10:09:39 +0800

    refactor 清理linux脚本中的ip

--- worker/ip_speed_test_linux.sh
+++ worker/ip_speed_test_linux.sh
@@ -5,17 +5,7 @@ TARGET_URL="https://cdn.steamusercontent.com/ugc/1852675168601248369/F20C8BCDE35
 HOSTNAME="cdn.steamusercontent.com"
 
 IPS=(
-  "23.59.72.59"
-  "23.59.72.42"
-  "23.206.175.162"
-  "23.206.175.170"
-  "23.55.51.221"
-  "23.55.51.217"
-  "23.67.75.97"
-  "23.67.75.74"
-  "23.219.172.57"
-  "23.219.172.51"
-  "203.69.138.225"
+
 )
 
 echo "Testing IPs from this server..."
